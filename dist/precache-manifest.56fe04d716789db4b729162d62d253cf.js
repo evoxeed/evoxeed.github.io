126a2f85fb2c3235bf522c000a696fd7f3c47c38
GomozovNikita <nikita.gomozov@nutnet.ru>
2021-09-23 16:12:58 +0400

    test porcshe-frontend

---
+++ dist/precache-manifest.56fe04d716789db4b729162d62d253cf.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07a10ea6eb756881583b",
+    "url": "/dist/css/app.db587220.css"
+  },
+  {
+    "revision": "c634a3a8efa5f9f96919",
+    "url": "/dist/css/chunk-vendors.201ea554.css"
+  },
+  {
+    "revision": "bbf888969bb66c388a12875b7d6f248c",
+    "url": "/dist/img/1.bbf88896.png"
+  },
+  {
+    "revision": "f0d5e83ebb5e53ca8746c7b2f9a75616",
+    "url": "/dist/img/2.f0d5e83e.png"
+  },
+  {
+    "revision": "35609e4691432b1d6f4abcb40024b189",
+    "url": "/dist/img/3.35609e46.png"
+  },
+  {
+    "revision": "e731b5c17467bfd16c3d557f6836d777",
+    "url": "/dist/img/4.e731b5c1.png"
+  },
+  {
+    "revision": "99948216da25fd615d436fd6f26ca4c0",
+    "url": "/dist/img/5.99948216.png"
+  },
+  {
+    "revision": "7dbf673bef924ee47b0631709d4c8165",
+    "url": "/dist/img/about.7dbf673b.png"
+  },
+  {
+    "revision": "4e096ae3fb4dd052d4a65f9ae1ee8854",
+    "url": "/dist/img/amazingteam.4e096ae3.png"
+  },
+  {
+    "revision": "ab9b6c031af82d519f861a2c9dbd2e1c",
+    "url": "/dist/img/css-sprites.ab9b6c03.png"
+  },
+  {
+    "revision": "e8c875faa7d50c0e4ce54f9e8c10ca66",
+    "url": "/dist/img/footer.e8c875fa.png"
+  },
+  {
+    "revision": "5613ccc20d8ecfef5eb3db378c2dc301",
+    "url": "/dist/img/header.5613ccc2.png"
+  },
+  {
+    "revision": "e9fdfe2f2fbd2a901b578294bb95b95a",
+    "url": "/dist/img/review.e9fdfe2f.png"
+  },
+  {
+    "revision": "36830cb0a0399e50f12484da671fa3cd",
+    "url": "/dist/img/video.36830cb0.png"
+  },
+  {
+    "revision": "1d08c20aa458124387b000a4ebd90e85",
+    "url": "/dist/index.html"
+  },
+  {
+    "revision": "07a10ea6eb756881583b",
+    "url": "/dist/js/app.5e514960.js"
+  },
+  {
+    "revision": "c634a3a8efa5f9f96919",
+    "url": "/dist/js/chunk-vendors.23ec553c.js"
+  },
+  {
+    "revision": "6ab6c0f03e3f24379dba2c157ec39834",
+    "url": "/dist/manifest.json"
+  },
+  {
+    "revision": "b7dd0785bb66f28323de7952b2e362ac",
+    "url": "/dist/media/menu-05-convert.b7dd0785.mp4"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/dist/robots.txt"
+  }
+]);
