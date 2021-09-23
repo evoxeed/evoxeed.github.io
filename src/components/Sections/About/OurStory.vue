e729c1f6ad2675d143cd12b1a0600d456d789f63
GomozovNikita <nikita.gomozov@nutnet.ru>
2021-09-23 14:18:27 +0400

    test video

--- src/components/Sections/About/OurStory.vue
+++ src/components/Sections/About/OurStory.vue
@@ -2,10 +2,10 @@
   <section class="about">
     <b-container class="inserts container-max-width">
       <b-row class="m-0">
-        <b-col cols="12" lg="5" class="text-center pb-4">
-          <img class="about__image" :src="require('@/assets/img/logos/about.png')" alt="about our story">
+        <b-col class="text-center pb-4" cols="12" lg="5">
+          <img :src="require('@/assets/img/logos/about.png')" alt="about our story" class="about__image">
         </b-col>
-        <b-col cols="12" lg="7" class="px-2">
+        <b-col class="px-2" cols="12" lg="7">
           <h2 class="heading heading-page text-uppercase m-0">our story</h2>
           <p class="about__text text">
             This is Photoshop's version of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean
@@ -22,6 +22,32 @@
           <button class="button button--active text-uppercase">learn more</button>
         </b-col>
       </b-row>
+    </b-container>
+    <b-container>
+      <b-col cols="12">
+        <h1>video-background</h1>
+        <video-background
+            :src="require(`@/assets/video/menu-05-convert.mp4`)"
+            muted
+            preload='auto'
+            style="max-height: 400px; height: 100vh; width: 1000px"
+        >
+        </video-background>
+      </b-col>
+      <b-col cols="12">
+        <h1>HTML 5 video</h1>
+        <video
+          controls
+          muted
+          playsinline
+          autoplay
+          loop
+          preload="auto"
+          width="1000"
+          >
+          <source :src="require(`@/assets/video/menu-05-convert.mp4`)" type="video/mp4">
+        </video>
+      </b-col>
     </b-container>
   </section>
 </template>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import 'mutationobserver-shim'
 import Vue from 'vue'
 import App from './App.vue'
 import './plugins/bootstrap-vue'
+import VideoBackground from 'vue-responsive-video-background-player'
 
+Vue.component('video-background', VideoBackground);
 Vue.config.productionTip = false
 
 new Vue({
